90827de6dbec26f3ea9dc01501e9a29d7614797d
abhijithabhi01 <iamabhijithabhi01@gmail.com>
2024-04-24 12:54:23 +0530

    first commit

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Chatbox from '../components/chatbox/Chatbox'
+
+function LandingPage() {
+  return (
+    <>
+<Chatbox/>
+    </>
+  )
+}
+
+export default LandingPage
